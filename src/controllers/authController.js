5a42a4b5f2a4a9e45acac4cfd9e403d4073eea60
Wilton Júnior <wilton.santos@accenture.com>
2018-09-24 06:23:10 -0300

    Criação da api rest utilizando mongo db

---
+++ src/controllers/authController.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/User');
+const authConfig = require('../config/auth.json');
+
+const router = express.Router();
+
+function generaterToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400
+    });
+}
+
+router.post('/register', async (req, res) => {
+
+    const { email } = req.body;
+
+    try {
+
+        if (await User.findOne({ email })) {
+            return res.status(400).send({ error: 'User already exists' })
+        }
+
+        const user = await User.create(req.body);
+
+        user.password = undefined;
+
+        return res.send({
+            user,
+            token: generaterToken({ id: user.id })
+        });
+
+    } catch (err) {
+
+        return res.status(400).send({ error: 'Resgistrations failed' })
+
+    }
+});
+
+router.post('/authenticate', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+        return res.status(400).send({ error: 'User not fount' })
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+        return res.status(400).send({ error: 'Invalid password' });
+    }
+
+    user.password = undefined;
+
+    res.send({
+        user,
+        token: generaterToken({ id: user.id })
+    });
+})
+
+module.exports = app => app.use('/auth', router);
